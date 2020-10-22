74a053b90cd98bedb778790da496882806989474
jihadul4kabar <jihadul4kbar@gmail.com>
2020-10-22 16:26:30 +0800

    upload all file

---
+++ lib/plugins/deletepagebutton/lang/en/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Delete Page Button plugin - English language file
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Damien Regad
+ */
+
+$lang['menu_item']  = 'Delete page';
+$lang['deleted_ok'] = 'Page deleted successfully';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = 'Are you sure you want to delete this page ?';
+$lang['js']['btn_ok']     = 'OK';
+$lang['js']['btn_cancel'] = 'Cancel';
